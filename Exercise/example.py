381721c3f0aebdf45e283af3d64e5cca500fff2e
computational physics_N2013301020145 <chenfengwhu@gmail.com>
2016-03-24 20:16:28 +0800

    整理文件

---
+++ Exercise/example.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 24 19:09:36 2016
+
+@author: AF
+"""
+
+import ode as solving_ode
+import matplotlib.pyplot as plt
+
+A = solving_ode.ode(0.1,0,2,5)  ##### setting the initial population N(0) = 10
+A.set_fx('N','t','N')
+eulerplus_record = A.euler()
+rgkt_3_record = A.rgkt_3()
+plt.figure(figsize = (10,6))
+plt.plot(eulerplus_record[0],eulerplus_record[1],'*',label = 'simple euler method')
+plt.plot(rgkt_3_record[0],rgkt_3_record[1],label = '3rd Runge-Kutta method')
+plt.legend()
+plt.savefig('chapter1_1.6.png',dpi = 144)
+plt.show()
