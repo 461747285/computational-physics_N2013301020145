f0427f5f500af69efabced892a862bf587b1a17c
computational physics_N2013301020145 <chenfengwhu@gmail.com>
2016-03-24 16:49:14 +0800

    bugs fixed

--- Exercise/Chapter 1/ode.py
+++ Exercise/Chapter 1/ode.py
@@ -2,6 +2,7 @@
 """
 Created on Thu Mar 17 14:49:37 2016
 @author: AF
+version 2.2
 """
 # this package involve three numerical methods to solve ordinary differential equation.
 # they are Euler method, Crome-Euler method and Runge-Kutta method
@@ -24,10 +25,7 @@ class ode:
     def set_fx(self,f,independent_v,dependent_v):                ###setting the function on the right side
         self.f = f
         self.number = len(dependent_v)
-        self.dep_name = []
-        self.dep_name.append(independent_v)
-        self.dep_name.append(dependent_v)
-        self.dep_name_record = self.dep_name[:]
+        self.f = self.f.replace(independent_v,'x').replace(dependent_v,'y')
         return 0    
     
     def set_fxs(self,f,independent_v,dependent_v):                ###setting the function on the right side for equation systems
@@ -47,8 +45,8 @@ class ode:
         self.y.append(self.Y)
         self.x.append(self.a)
         for i in range(1,self.N):
-            self.dep_name[1] = self.y[i - 1]
-            self.dep_name[0] = self.x[i - 1]
+            y = self.y[i - 1]
+            x = self.x[i - 1]
             self.y.append(self.y[i - 1] + eval(self.f) * self.dx)
             self.x.append(self.x[i - 1] + self.dx)
         self.data[0] = self.x
@@ -62,10 +60,10 @@ class ode:
         self.y.append(self.Y)
         self.x.append(self.a)
         for i in range(1,self.N):
-            self.dep_name[1] = self.y[i - 1]
-            self.dep_name[0] = self.x[i - 1]
+            y = self.y[i - 1]
+            x = self.x[i - 1]
             v1 = eval(self.f)
-            self.dep_name[1] = self.y[i - 1] + v1*self.dx
+            y = self.y[i - 1] + v1*self.dx
             v2 = eval(self.f)
             self.y.append(self.y[i - 1] + self.dx * (v1 + v2)/2)
             self.x.append(self.x[i - 1] + self.dx)
@@ -80,11 +78,11 @@ class ode:
         self.y.append(self.Y)
         self.x.append(self.a)
         for i in range(1,self.N):
-            self.dep_name[1] = self.y[i - 1]
-            self.dep_name[0] = self.x[i - 1]
+            y = self.y[i - 1]
+            x = self.x[i - 1]
             v1 = eval(self.f)
-            self.dep_name[1] = self.y[i - 1] + v1*self.dx/2
-            self.dep_name[0] = self.x[i - 1] + self.dx/2
+            y = self.y[i - 1] + v1*self.dx/2
+            x = self.x[i - 1] + self.dx/2
             v2 = eval(self.f)
             self.y.append(self.y[i - 1] + self.dx * v2)
             self.x.append(self.x[i - 1] + self.dx)
@@ -99,14 +97,14 @@ class ode:
         self.y.append(self.Y)
         self.x.append(self.a)
         for i in range(1,self.N):
-            self.dep_name[1] = self.y[i - 1]
-            self.dep_name[0] = self.x[i - 1]
+            y = self.y[i - 1]
+            x = self.x[i - 1]
             k1 = eval(self.f)
-            self.dep_name[0] = self.x[i - 1] + self.dx/2
-            self.dep_name[1] = self.y[i - 1] + k1*self.dx/2
+            x = self.x[i - 1] + self.dx/2
+            y = self.y[i - 1] + k1*self.dx/2
             k2 = eval(self.f)
-            self.dep_name[0] = self.x[i - 1] + self.dx
-            self.dep_name[1] = self.y[i - 1] - k1*self.dx + 2*k2*self.dx
+            x = self.x[i - 1] + self.dx
+            y = self.y[i - 1] - k1*self.dx + 2*k2*self.dx
             k3 = eval(self.f)
             self.y.append(self.y[i - 1] + self.dx * (k1 + 4*k2 +k3)/6)
             self.x.append(self.x[i - 1] + self.dx)
@@ -115,7 +113,6 @@ class ode:
         return self.data  
         
     def eulers(self):                   ###calculate equations via Euler method
-#        tick_1 = time.time()
         self.x.append(self.a)           #
         for l in range(self.number):    ###
             self.y.append([])           ####    setting the initial valye
@@ -131,11 +128,9 @@ class ode:
             self.x.append(self.x[i - 1] + self.dx)
         self.data[0] = self.x
         self.data[1] = self.y
-#        print (time.time()-tick_1)
         return self.data
         
     def rgkt_3s(self):                  ### third order Runge_Kutta for equation systems
-#        tick_1 = time.time()
         self.x.append(self.a)           #
         for l in range(self.number):    ###
             self.y.append([])           ####    setting the initial valye
@@ -162,11 +157,19 @@ class ode:
             self.x.append(self.x[i - 1] + self.dx)
         self.data[0] = self.x
         self.data[1] = self.y
-#        print (time.time()-tick_1)
         return self.data 
     
-    def store(self):
+    def store(self):                 ### for ode
         data_file = open('data_ode.txt','w')
+        for i in range(self.N):
+            for j in range(2):
+                data_file.write(str(self.data[j][i]))
+                data_file.write(' ')
+            data_file.write('\n')
+        data_file.close    
+    
+    def stores(self):           #### for odes                
+        data_file = open('data_odes.txt','w')
         for i in range(self.N):
             data_file.write(str(self.data[0][i]))
             data_file.write(' ')
@@ -177,8 +180,9 @@ class ode:
         data_file.close  
     
     
-A = ode(0.05,0,5,(100,0))
-A.set_fxs(['y2-y1','y1-y2'],'x',('y1','y2'))
+A = ode(0.05,0,1,1)
+#A.set_fxs(['y2-y1','y1-y2'],'x',('y1','y2'))
+A.set_fx('y','x','y')
 #A.euler()
 #A.eulerplus()
 #A.rgkt_2()  
@@ -186,10 +190,12 @@ A.set_fxs(['y2-y1','y1-y2'],'x',('y1','y2'))
 #plt.figure(figsize=(10,6),dpi = 144)
 record = []
 #record = A.eulers()
-record = A.rgkt_3s()
+#record = A.rgkt_3s()
+#record = A.eulerplus()
+record = A.rgkt_3()
 A.store()
-plt.plot(record[0],record[1][0], label ='particle a')
-plt.plot(record[0],record[1][1], label ='particle b')
+plt.plot(record[0],record[1], label ='particle a')
+#plt.plot(record[0],record[1][1], label ='particle b')
 plt.legend()
 plt.savefig('ode.png',dpi=144)  
 plt.show()      
