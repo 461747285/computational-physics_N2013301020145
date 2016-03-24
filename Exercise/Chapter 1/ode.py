e882230f236b0ab67beb3c6d4f068e2a0c63b831
computational physics_N2013301020145 <chenfengwhu@gmail.com>
2016-03-24 13:35:23 +0800

    重大升级
    1.加入新函数，分别是用简单欧拉法和龙格法解微分方程组
    2.取消自带画图功能

--- Exercise/Chapter 1/ode.py
+++ Exercise/Chapter 1/ode.py
@@ -10,124 +10,182 @@ Created on Thu Mar 17 14:49:37 2016
 import matplotlib.pyplot as plt
 
 class ode:
-    def __init__(self,f,dx,a,b,Y):
-        self.f = f      # the function on the right side 
+    def __init__(self,dx,a,b,Y):
         self.dx = dx    # step length
         self.a = a      # lower limit
         self.b = b      # upper limit
         self.Y = Y      # initial y
-        self.data = [[],[],[],[],[]] # store all the datas
-    
-    def input_fx(self):                ###input the function on the right side
-        self.f = raw_input('please input the expression on the right side (x,y):')
-        independent_v = raw_input('please input name of the independent variable of the expression:')
-        dependent_v = raw_input('please input the name of the dependent variable of the expression:')
-        self.f = self.f.replace(independent_v,'x').replace(dependent_v,'y')
-        print self.f
-    
-    def euler(self):                   ###calculate via Euler method
         self.y = []
         self.x = []
+        self.N = int(float(self.b - self.a)/(self.dx))
+        self.data = [[],[]] # store all the datas
+        
+    def set_fx(self,f,independent_v,dependent_v):                ###setting the function on the right side
+        self.f = f
+        self.number = len(dependent_v)
+        self.dep_name = []
+        self.dep_name.append(independent_v)
+        self.dep_name.append(dependent_v)
+        self.dep_name_record = self.dep_name[:]
+        return 0    
+    
+    def set_fxs(self,f,independent_v,dependent_v):                ###setting the function on the right side for equation systems
+        self.f = f
+        self.number = len(dependent_v)
+        self.dep_name = []
+        self.dep_name.append(independent_v)
+        for i in range(self.number):
+            self.dep_name.append(dependent_v[i])
+        self.dep_name_record = self.dep_name[:]
+        return 0
+    
+    def euler(self):                   ###calculate via Euler method
+        if (self.number != 1):
+            print 'this method cannot solve ode equation systems.'
+            exit()
         self.y.append(self.Y)
         self.x.append(self.a)
-        self.N = int(float(self.b - self.a)/(self.dx))
         for i in range(1,self.N):
-            y = self.y[i - 1]
-            x = self.x[i - 1]
+            self.dep_name[1] = self.y[i - 1]
+            self.dep_name[0] = self.x[i - 1]
             self.y.append(self.y[i - 1] + eval(self.f) * self.dx)
             self.x.append(self.x[i - 1] + self.dx)
         self.data[0] = self.x
         self.data[1] = self.y
-        return 0 
+        return self.data
         
     def eulerplus(self):                 ### modified euler method
-        self.y = []
-        self.x = []
+        if (self.number != 1):
+            print 'this method cannot solve ode equation systems.'
+            exit()
         self.y.append(self.Y)
         self.x.append(self.a)
-        self.N = int(float(self.b - self.a)/(self.dx))
         for i in range(1,self.N):
-            y = self.y[i - 1]
-            x = self.x[i - 1]
+            self.dep_name[1] = self.y[i - 1]
+            self.dep_name[0] = self.x[i - 1]
             v1 = eval(self.f)
-            y = self.y[i - 1] + v1*self.dx
+            self.dep_name[1] = self.y[i - 1] + v1*self.dx
             v2 = eval(self.f)
             self.y.append(self.y[i - 1] + self.dx * (v1 + v2)/2)
             self.x.append(self.x[i - 1] + self.dx)
         self.data[0] = self.x
-        self.data[2] = self.y
-        return 0
+        self.data[1] = self.y
+        return self.data
         
     def rgkt_2(self):                    ### second order Runge_Kutta
-        self.y = []
-        self.x = []
+        if (self.number != 1):
+            print 'this method cannot solve ode equation systems.'
+            exit()
         self.y.append(self.Y)
         self.x.append(self.a)
-        self.N = int(float(self.b - self.a)/(self.dx))
         for i in range(1,self.N):
-            y = self.y[i - 1]
-            x = self.x[i - 1]
+            self.dep_name[1] = self.y[i - 1]
+            self.dep_name[0] = self.x[i - 1]
             v1 = eval(self.f)
-            y = self.y[i - 1] + v1*self.dx/2
-            x = self.x[i - 1] + self.dx/2
+            self.dep_name[1] = self.y[i - 1] + v1*self.dx/2
+            self.dep_name[0] = self.x[i - 1] + self.dx/2
             v2 = eval(self.f)
             self.y.append(self.y[i - 1] + self.dx * v2)
             self.x.append(self.x[i - 1] + self.dx)
         self.data[0] = self.x
-        self.data[3] = self.y
-        return 0
+        self.data[1] = self.y
+        return self.data
     
     def rgkt_3(self):                    ### third order Runge_Kutta
-        self.y = []
-        self.x = []
+        if (self.number != 1):
+            print 'this method cannot solve ode equation systems.'
+            exit()
         self.y.append(self.Y)
         self.x.append(self.a)
-        self.N = int(float(self.b - self.a)/(self.dx))
         for i in range(1,self.N):
-            y = self.y[i - 1]
-            x = self.x[i - 1]
+            self.dep_name[1] = self.y[i - 1]
+            self.dep_name[0] = self.x[i - 1]
             k1 = eval(self.f)
-            x = self.x[i - 1] + self.dx/2
-            y = self.y[i - 1] + k1*self.dx/2
+            self.dep_name[0] = self.x[i - 1] + self.dx/2
+            self.dep_name[1] = self.y[i - 1] + k1*self.dx/2
             k2 = eval(self.f)
-            x = self.x[i - 1] + self.dx
-            y = self.y[i - 1] - k1*self.dx + 2*k2*self.dx
+            self.dep_name[0] = self.x[i - 1] + self.dx
+            self.dep_name[1] = self.y[i - 1] - k1*self.dx + 2*k2*self.dx
             k3 = eval(self.f)
             self.y.append(self.y[i - 1] + self.dx * (k1 + 4*k2 +k3)/6)
             self.x.append(self.x[i - 1] + self.dx)
         self.data[0] = self.x
-        self.data[4] = self.y
-        return 0   
+        self.data[1] = self.y
+        return self.data  
+        
+    def eulers(self):                   ###calculate equations via Euler method
+        self.x.append(self.a)           #
+        for l in range(self.number):    ###
+            self.y.append([])           ####    setting the initial valye
+            self.y[l].append(self.Y[l]) ###
+        for i in range(1,self.N):
+            for j in range(self.number):
+                for k in range(self.number):
+                    self.dep_name[k + 1] = self.dep_name_record[k + 1] + '=' + str(self.y[k][i - 1])
+                self.dep_name[0] = self.dep_name_record[0] + '=' + str(self.x[i - 1])
+                for m in range(self.number + 1):
+                    exec self.dep_name[m]
+                self.y[j].append(self.y[j][i - 1] + eval(self.f[j]) * self.dx)
+            self.x.append(self.x[i - 1] + self.dx)
+        self.data[0] = self.x
+        self.data[1] = self.y
+        return self.data
+        
+    def rgkt_3s(self):                  ### third order Runge_Kutta for equation systems
+        self.x.append(self.a)           #
+        for l in range(self.number):    ###
+            self.y.append([])           ####    setting the initial valye
+            self.y[l].append(self.Y[l]) ###
+        for i in range(1,self.N):
+            for j in range(self.number):
+                for d in range(self.number):
+                    self.dep_name[d + 1] = self.dep_name_record[d + 1] + '=' + str(self.y[d][i - 1])
+                self.dep_name[0] = self.dep_name_record[0] + '=' + str(self.x[i - 1])
+                for m in range(self.number + 1):
+                    exec self.dep_name[m]
+                k1 = eval(self.f[j])
+                for e in range(self.number):
+                    self.dep_name[e + 1] = self.dep_name_record[e + 1] + '=' + str(self.y[e][i - 1] + k1*self.dx/2)
+                self.dep_name[0] = self.dep_name_record[0] + '=' + str(self.x[i - 1]+self.dx/2)
+                for n in range(self.number + 1):
+                    exec self.dep_name[n]
+                k2 = eval(self.f[j])
+                for f in range(self.number):
+                    self.dep_name[f + 1] = self.dep_name_record[f + 1] + '=' + str(self.y[f][i - 1] - k1*self.dx + 2*k2*self.dx)
+                self.dep_name[0] = self.dep_name_record[0] + '=' + str(self.x[i - 1] + self.dx)
+                for o in range(self.number + 1):
+                    exec self.dep_name[o]
+                k3 = eval(self.f[j])
+                self.y[j].append(self.y[j][i - 1] + self.dx * (k1 + 4*k2 +k3)/6)
+            self.x.append(self.x[i - 1] + self.dx)
+        self.data[0] = self.x
+        self.data[1] = self.y
+        return self.data 
     
     def store(self):
         data_file = open('data_ode.txt','w')
         for i in range(self.N):
-            for j in range(4):
-                data_file.write(str(self.data[j][i]))
+            data_file.write(str(self.data[0][i]))
+            data_file.write(' ')
+            for j in range(len(self.data[1])):
+                data_file.write(str(self.data[1][j][i]))
                 data_file.write(' ')
             data_file.write('\n')
         data_file.close  
     
-    def draw_figure(self,j):
-        notation = {'1': 'Euler_method','2':'Modified_Euler_method','3':'Second_order_Runge_Kutta method','4':'Third_order_Runge_Kutta method'}
-        if (j == 1):
-            plt.plot(self.data[0],self.data[j],label = notation[str(j)])
-        elif(j == 2):
-            plt.plot(self.data[0],self.data[j],label = notation[str(j)])
-        elif(j == 3):
-            plt.plot(self.data[0],self.data[j],label = notation[str(j)])
-        elif(j == 4):
-            plt.plot(self.data[0],self.data[j],label = notation[str(j)])
     
-A = ode('x',0.05,0,5,100)
-A.input_fx()
-A.euler()
-A.eulerplus()
-A.rgkt_2()  
-A.rgkt_3()
-plt.figure(figsize=(10,6),dpi = 144)
-for i in range(4):
-    A.draw_figure(i+1)
+A = ode(0.05,0,5,(100,0))
+A.set_fxs(['y2-y1','y1-y2'],'x',('y1','y2'))
+#A.euler()
+#A.eulerplus()
+#A.rgkt_2()  
+#A.rgkt_3()
+#plt.figure(figsize=(10,6),dpi = 144)
+record = []
+record = A.rgkt_3s()
+A.store()
+plt.plot(record[0],record[1][0], label ='particle a')
+plt.plot(record[0],record[1][1], label ='particle b')
 plt.legend()
 plt.savefig('ode.png',dpi=144)  
 plt.show()      
