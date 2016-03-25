acf70465e4549171b8d608556d740cde3cb8df32
chenfeng2013301020145 <chenfengwhu@gmail.com>
2016-03-25 14:26:33 +0800

    Update example2.py

--- ode_package/example2.py
+++ ode_package/example2.py
@@ -9,13 +9,13 @@ import ode as solving_ode
 import matplotlib.pyplot as plt
 import math
 
-A = solving_ode.ode(0.005,3.8,4,(2,8))  ##### setting the initial population N(0) = 10
-A.set_fxs(('y1','-x*y+y1*math.e**x+3*math.sin(2*x)'),'x',('y','y1'))
-eulers_record = A.eulers()
-rgkt_3s_record = A.rgkt_3s()
+A = solving_ode.ode(0.005,3.8,4,(2,8))  
+A.set_fxs(('y1','-x*y+y1*math.e**x+3*math.sin(2*x)'),['x','y','y1'])
+euler_record = A.euler()
+rgkt_3_record = A.rgkt_3()
 plt.figure(figsize = (10,6))
-plt.plot(eulers_record[0],eulers_record[1][0],'*',label = 'simple euler method')
-plt.plot(rgkt_3s_record[0],rgkt_3s_record[1][0],label = '3rd Runge-Kutta method')
+plt.plot(euler_record[0],euler_record[1][0],'*',label = 'simple euler method')
+plt.plot(rgkt_3_record[0],rgkt_3_record[1][0],label = '3rd Runge-Kutta method')
 plt.legend(loc = 'left upper')
 plt.savefig('example2.png',dpi = 144)
 plt.show()
