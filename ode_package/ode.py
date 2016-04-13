aed96673c7ea2cf89161cdb448b53057ed96ef89
computational physics_N2013301020145 <chenfengwhu@gmail.com>
2016-04-13 16:13:04 +0800

    Bug fix；增加4阶龙格函数rgkt_4

--- ode_package/ode.py
+++ ode_package/ode.py
@@ -2,7 +2,7 @@
 """
 Created on Thu Mar 17 14:49:37 2016
 @author: AF
-version 3.1
+version 4.0
 """
 # this package involve three numerical methods to solve ordinary differential equation.
 # they are Euler method, Crome-Euler method and Runge-Kutta method
@@ -10,6 +10,7 @@ version 3.1
 
 import math
 import numpy
+
 
 class ode:
     def __init__(self,dx,a,b,Y):
@@ -35,103 +36,143 @@ class ode:
             self.dep_name.append(variable_name[i])
         self.dep_name_record = self.dep_name[:]
         return 0
+    
+    def Exe(self,string):
+        pass
         
     def euler(self):                   ###simple Euler method
         self.y = []
         self.x = []
         self.x.append(self.a)           #
         for l in range(self.number):    ###
-            self.y.append([])           ####    setting the initial valye
+            self.y.append([])           ####    setting the initial value
             self.y[l].append(self.Y[l]) ###
         for i in range(1,self.N):
+            for k in range(self.number):
+                self.dep_name[k + 1] = self.dep_name_record[k + 1] + '=' + str(self.y[k][i - 1])
+            self.dep_name[0] = self.dep_name_record[0] + '=' + str(self.x[i - 1])
+            for m in range(self.number + 1):
+                exec self.dep_name[m]
             for j in range(self.number):
-                for k in range(self.number):
-                    self.dep_name[k + 1] = self.dep_name_record[k + 1] + '=' + str(self.y[k][i - 1])
-                self.dep_name[0] = self.dep_name_record[0] + '=' + str(self.x[i - 1])
-                for m in range(self.number + 1):
-                    exec self.dep_name[m]
                 self.y[j].append(self.y[j][i - 1] + eval(self.f[j]) * self.dx)
             self.x.append(self.x[i - 1] + self.dx)
         self.data[0] = self.x
         self.data[1] = self.y
         return self.data
         
-    def eulerplus(self):                 ### modified euler method 
+    def rgkt_2(self):                    ### second-order Runge_Kutta
         self.y = []
         self.x = []
+        v1 = [0]*self.number
+        v2 = [0]*self.number
         self.x.append(self.a)
         for l in range(self.number):    ##
-            self.y.append([])           ####   setting the initial valye
+            self.y.append([])           ####   setting the initial value
             self.y[l].append(self.Y[l]) ##
+        print self.number
         for i in range(1,self.N):
+            for n in range(self.number):
+                self.dep_name[n + 1] = self.dep_name_record[n + 1] + '=' + str(self.y[n][i - 1])
+            self.dep_name[0] = self.dep_name_record[0] + '=' + str(self.x[i - 1])
+            for m in range(self.number + 1):
+                exec self.dep_name[m]
             for j in range(self.number):
-                for k in range(self.number):
-                    self.dep_name[k + 1] = self.dep_name_record[k + 1] + '=' + str(self.y[k][i - 1])
-                self.dep_name[0] = self.dep_name_record[0] + '=' + str(self.x[i - 1])
-                for m in range(self.number + 1):
-                    exec self.dep_name[m]
-                v1 = eval(self.f)
-                self.dep_name[j + 1] = self.dep_name_record[j + 1] + '=' + str(self.y[j][i - 1] + v1*self.dx)
-                exec self.dep_name[j + 1]
-                v2 = eval(self.f)
-                self.y[j].append(self.y[j][i - 1] + self.dx * (v1 + v2)/2)
-            self.x.append(self.x[i - 1] + self.dx)
-        self.data[0] = self.x
-        self.data[1] = self.y
-        return self.data
-        
-    def rgkt_2(self):                    ### second order Runge_Kutta
-        self.y = []
-        self.x = []
-        self.x.append(self.a)
-        for l in range(self.number):    ##
-            self.y.append([])           ####   setting the initial valye
-            self.y[l].append(self.Y[l]) ##
-        for i in range(1,self.N):
+                v1[j] = eval(self.f[j])
+            for k in range(self.number):
+                self.dep_name[k + 1] = self.dep_name_record[k + 1] + '=' + str(self.y[k][i - 1] + v1[k]*self.dx/2)
+            self.dep_name[0] = self.dep_name_record[0] + '=' + str(self.x[i - 1] + self.dx/2)
+            for m in range(self.number + 1):
+                exec self.dep_name[m]
             for j in range(self.number):
-                for k in range(self.number):
-                    self.dep_name[k + 1] = self.dep_name_record[k + 1] + '=' + str(self.y[k][i - 1])
-                self.dep_name[0] = self.dep_name_record[0] + '=' + str(self.x[i - 1])
-                for m in range(self.number + 1):
-                    exec self.dep_name[m]
-                v1 = eval(self.f)
-                self.dep_name[j + 1] = self.dep_name_record[j + 1] + '=' + str(self.y[j][i - 1] + v1*self.dx/2)
-                self.dep_name[0] = self.dep_name_record[0] + '=' + str(self.x[i - 1] + self.dx/2)
-                exec self.dep_name[0]
-                exec self.dep_name[j + 1]
-                v2 = eval(self.f)
-                self.y[j].append(self.y[j][i - 1] + self.dx * v2)
+                v2[j] = eval(self.f[j])
+            for k in range(self.number):
+                self.y[k].append(self.y[k][i - 1] + self.dx * v2[k])
             self.x.append(self.x[i - 1] + self.dx)
         self.data[0] =self.x
         self.data[1] =self.y
         return self.data
         
-    def rgkt_3(self):                  ### third order Runge_Kutta 
+    def rgkt_3(self):                  ### third-order Runge_Kutta 
         self.y = []
         self.x = []
+        k1 = [0]*self.number
+        k2 = [0]*self.number
+        k3 = [0]*self.number
         self.x.append(self.a)           #
         for l in range(self.number):    ###
-            self.y.append([])           ####    setting the initial valye
+            self.y.append([])           ####    setting the initial value
             self.y[l].append(self.Y[l]) ###
         for i in range(1,self.N):
+            for d in range(self.number):
+                self.dep_name[d + 1] = self.dep_name_record[d + 1] + '=' + str(self.y[d][i - 1])
+            self.dep_name[0] = self.dep_name_record[0] + '=' + str(self.x[i - 1])
+            for m in range(self.number + 1):
+                exec self.dep_name[m]
             for j in range(self.number):
-                for d in range(self.number):
-                    self.dep_name[d + 1] = self.dep_name_record[d + 1] + '=' + str(self.y[d][i - 1])
-                self.dep_name[0] = self.dep_name_record[0] + '=' + str(self.x[i - 1])
-                for m in range(self.number + 1):
-                    exec self.dep_name[m]
-                k1 = eval(self.f[j])
-                self.dep_name[j + 1] = self.dep_name_record[j + 1] + '=' + str(self.y[j][i - 1] + k1*self.dx/2)
-                self.dep_name[0] = self.dep_name_record[0] + '=' + str(self.x[i - 1]+self.dx/2)
-                exec self.dep_name[0]
-                exec self.dep_name[j + 1]
-                k2 = eval(self.f[j])
-                self.dep_name[j + 1] = self.dep_name_record[j + 1] + '=' + str(self.y[j][i - 1] - k1*self.dx + 2*k2*self.dx)
-                self.dep_name[0] = self.dep_name_record[0] + '=' + str(self.x[i - 1] + self.dx)
-                exec self.dep_name[0]
-                exec self.dep_name[j + 1]
-                k3 = eval(self.f[j])
-                self.y[j].append(self.y[j][i - 1] + self.dx * (k1 + 4*k2 +k3)/6)
+                k1[j] = eval(self.f[j])
+            for k in range(self.number):
+                self.dep_name[k + 1] = self.dep_name_record[k + 1] + '=' + str(self.y[k][i - 1] + k1[k]*self.dx/2)
+            self.dep_name[0] = self.dep_name_record[0] + '=' + str(self.x[i - 1] + self.dx/2)
+            for m in range(self.number + 1):
+                exec self.dep_name[m]
+            for j in range(self.number):
+                k2[j] = eval(self.f[j])
+            for k in range(self.number):
+                self.dep_name[k + 1] = self.dep_name_record[k + 1] + '=' + str(self.y[k][i - 1] - k1[k]*self.dx + 2*k2[k]*self.dx)
+            self.dep_name[0] = self.dep_name_record[0] + '=' + str(self.x[i - 1] + self.dx/2)
+            for m in range(self.number + 1):
+                exec self.dep_name[m]
+            for j in range(self.number):
+                k3[j] = eval(self.f[j])
+            for k in range(self.number):
+                self.y[k].append(self.y[k][i - 1] + self.dx * (k1[k] + 4*k2[k] +k3[k])/6)
+            self.x.append(self.x[i - 1] + self.dx)
+        self.data[0] = self.x
+        self.data[1] = self.y
+        return self.data 
+ 
+    def rgkt_4(self):                  ### forth-order Runge_Kutta 
+        self.y = []
+        self.x = []
+        k1 = [0]*self.number
+        k2 = [0]*self.number
+        k3 = [0]*self.number
+        k4 = [0]*self.number
+        self.x.append(self.a)           #
+        for l in range(self.number):    ###
+            self.y.append([])           ####    setting the initial value
+            self.y[l].append(self.Y[l]) ###
+        for i in range(1,self.N):
+            for d in range(self.number):
+                self.dep_name[d + 1] = self.dep_name_record[d + 1] + '=' + str(self.y[d][i - 1])
+            self.dep_name[0] = self.dep_name_record[0] + '=' + str(self.x[i - 1])
+            for m in range(self.number + 1):
+                exec self.dep_name[m]
+            for j in range(self.number):
+                k1[j] = eval(self.f[j])
+            for k in range(self.number):
+                self.dep_name[k + 1] = self.dep_name_record[k + 1] + '=' + str(self.y[k][i - 1] + 0.5*k1[k]*self.dx)
+            self.dep_name[0] = self.dep_name_record[0] + '=' + str(self.x[i - 1] + self.dx/2)
+            for m in range(self.number + 1):
+                exec self.dep_name[m]
+            for j in range(self.number):
+                k2[j] = eval(self.f[j])
+            for k in range(self.number):
+                self.dep_name[k + 1] = self.dep_name_record[k + 1] + '=' + str(self.y[k][i - 1] + 0.5*k2[k]*self.dx)
+            self.dep_name[0] = self.dep_name_record[0] + '=' + str(self.x[i - 1] + self.dx/2)
+            for m in range(self.number + 1):
+                exec self.dep_name[m]
+            for j in range(self.number):
+                k3[j] = eval(self.f[j])
+            for k in range(self.number):
+                self.dep_name[k + 1] = self.dep_name_record[k + 1] + '=' + str(self.y[k][i - 1] + k3[k]*self.dx)
+            self.dep_name[0] = self.dep_name_record[0] + '=' + str(self.x[i - 1] + self.dx)
+            for m in range(self.number + 1):
+                exec self.dep_name[m]
+            for j in range(self.number):
+                k4[j] = eval(self.f[j])
+            for k in range(self.number):
+                self.y[k].append(self.y[k][i - 1] + self.dx * (k1[k] + 2*k2[k] + 2*k3[k] + k4[k])/6)
             self.x.append(self.x[i - 1] + self.dx)
         self.data[0] = self.x
         self.data[1] = self.y
