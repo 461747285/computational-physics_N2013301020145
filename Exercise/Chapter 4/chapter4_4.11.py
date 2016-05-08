fdb59e1680e40fe03078a3fc4f81ef67a545ead0
computational physics_N2013301020145 <chenfengwhu@gmail.com>
2016-05-08 18:45:43 +0800

    4

--- Exercise/Chapter 4/chapter4_4.11.py
+++ Exercise/Chapter 4/chapter4_4.11.py
@@ -8,15 +8,28 @@ Created on Wed May  4 17:11:36 2016
 import numpy as np
 import matplotlib.pyplot as plt
 import ode as solving_ode
-from visual import *
-
+#from visual import *
 
 ###  -------- circular orbit -----------------
-A = solving_ode.ode(0.0001,0,1,(0.47,0,0,8.2))
-A.set_fx(['vx','-4*np.pi**2*x/(np.sqrt(x**2+y**2))**3*(1+0.01/(np.sqrt(x**2+y**2))**2)','vy','-4*np.pi**2*y/(np.sqrt(x**2+y**2))**3*(1+0.01/(np.sqrt(x**2+y**2))**2)'],['t','x','vx','y','vy'])
+e = 0.9
+r_min = 0.314
+r_0 = r_min*(1 + e)/(1 - e)
+a = r_min/(1 - e)
+v_0 = np.sqrt(4*np.pi**2*(1 - e)**2/(r_min*(1 + e))) 
+A = solving_ode.ode(0.001,0,25,(r_0,0,0,v_0))
+A.set_fx(['vx','-4*np.pi**2*x/(np.sqrt(x**2+y**2))**3*(1+0.001/(np.sqrt(x**2+y**2))**2)','vy','-4*np.pi**2*y/(np.sqrt(x**2+y**2))**3*(1+0.001/(np.sqrt(x**2+y**2))**2)'],['t','x','vx','y','vy'])
 data_record = A.rgkt_4()
+theta_record = []
+time_record = []
+for i in range(len(data_record[1][0]) - 1):
+    if ( data_record[1][0][i]**2+data_record[1][2][i]**2 > data_record[1][0][i - 1]**2+data_record[1][2][i - 1]**2 and data_record[1][0][i]**2+data_record[1][2][i]**2> data_record[1][0][i + 1]**2+data_record[1][2][i + 1]**2):
+        time_record.append(data_record[0][i])
+        theta_record.append(np.arctan(data_record[1][2][i]/data_record[1][0][i])*180/np.pi)
+print time_record
+print theta_record
+
 ###  ------ vpython -------------------------
-sun = sphere(pos = (0,0,0),radius = 0.22,color = color.yellow)
+'''sun = sphere(pos = (0,0,0),radius = 0.22,color = color.yellow)
 lamp = local_light(pos=(0,0,0), color=color.yellow)
 deltat = 0.0001
 i = 0
@@ -30,7 +43,7 @@ while i < len(data_record[1][1]):
     mercury.trail.append(pos = mercury.pos)
     t = t + deltat
     i+=1
-
+'''
 ### -----------------------------------------
 '''plt.figure(figsize = (10,8))
 plt.xlabel('x(AU)')
@@ -56,21 +69,23 @@ plt.show()'''
 ###  -------Precession: elliptical orbit ---------------- ## r_max = 0.47 AU Kappa = 2.6579 r_min = 0.314AU alpha = 0.01  period = 0.239
 #plt.figure(figsize = (16,12))
 #plt.subplot(111,polar = True)
-'''e = 0.206
+'''e = 0.2
 r_min = 0.314
 r_0 = r_min*(1 + e)/(1 - e)
 A = solving_ode.ode(0.01,0,30*np.pi,(r_0,0))  
 #print 1./(r_min*(1 + e))
 A.set_fx(['v','(2.0/r)*v**2+r-'+str(1./(r_min*(1 + e)))+'*(1 + 0.001/(r**2))*r**2'],['t','r','v'])
 #A.set_fx(['v','(2.0/r)*v**2+r-2.6579*(1 + 0.01/(r**2))*r**2'],['t','r','v'])
-data_record = A.rgkt_4()'''
-'''theta_record = []
+data_record = A.rgkt_4()
+theta_record = []
 #r_0 = 0.47
+print r_0
 for i in range(len(data_record[1][0]) - 1):
     if ( data_record[1][0][i] > data_record[1][0][i - 1] and data_record[1][0][i] > data_record[1][0][i + 1]):
         theta_record.append(data_record[0][i])
     if ( data_record[1][0][i] < data_record[1][0][i - 1] and data_record[1][0][i] < data_record[1][0][i + 1]):
-        print data_record[1][0][i]
+        #print data_record[1][0][i]
+        pass
 print theta_record'''
 '''eccentricities = [0.2,0.5,0.8]
 for i in range(3):
@@ -82,7 +97,7 @@ for i in range(3):
     data_record = A.rgkt_4()
     plt.plot(data_record[0],data_record[1][0],lw=1,label = 'e = '+str(eccentricities[i]))    
 plt.title('Simulation of the precession of Mercury')
-plt.legend()
-plt.savefig('chapter4_4.11_0.2.png',dpi = 144)
-plt.show()'''
+plt.legend()'''
+plt.savefig('chapter4_4.11.png',dpi = 144)
+plt.show()
 
---
+++ Exercise/Chapter 4/ode.py
@@ -0,0 +1,193 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 17 14:49:37 2016
+@author: AF
+version 4.01
+"""
+# this package involve three numerical methods to solve ordinary differential equation.
+# they are Euler method, Crome-Euler method and Runge-Kutta method
+# reference MATLAB 语言常用算法程序集（第2版）
+
+import math
+import numpy as np
+
+
+class ode:
+    def __init__(self,dx,a,b,Y):
+        self.dx = dx    # step length
+        self.a = a      # lower limit
+        self.Y = [0]
+        if (type(Y) == int):
+            self.Y[0] =Y
+        else:
+            self.Y = Y      # initial y
+        self.N = int(float(b - self.a)/(self.dx)) + 1
+        self.data = [[],[]] # store all the datas
+           
+    def set_fx(self,f,variable_name):                ###setting the function on the right side 
+        self.f = [0]
+        if (type(f) == str):
+            self.f[0] = f
+        else:
+            self.f = f
+        self.number = len(variable_name) - 1
+        self.dep_name = []
+        for i in range(len(variable_name)):
+            self.dep_name.append(variable_name[i])
+        self.dep_name_record = self.dep_name[:]
+        return 0
+    
+    def Exe(self,string):
+        pass
+        
+    def euler(self):                   ###simple Euler method
+        self.y = []
+        self.x = []
+        self.x.append(self.a)           #
+        for l in range(self.number):    ###
+            self.y.append([])           ####    setting the initial value
+            self.y[l].append(self.Y[l]) ###
+        for i in range(1,self.N):
+            for k in range(self.number):
+                self.dep_name[k + 1] = self.dep_name_record[k + 1] + '=' + str(self.y[k][i - 1])
+            self.dep_name[0] = self.dep_name_record[0] + '=' + str(self.x[i - 1])
+            for m in range(self.number + 1):
+                exec self.dep_name[m]
+            for j in range(self.number):
+                self.y[j].append(self.y[j][i - 1] + eval(self.f[j]) * self.dx)
+            self.x.append(self.x[i - 1] + self.dx)
+        self.data[0] = self.x
+        self.data[1] = self.y
+        return self.data
+        
+    def rgkt_2(self):                    ### second-order Runge_Kutta
+        self.y = []
+        self.x = []
+        v1 = [0]*self.number
+        v2 = [0]*self.number
+        self.x.append(self.a)
+        for l in range(self.number):    ##
+            self.y.append([])           ####   setting the initial value
+            self.y[l].append(self.Y[l]) ##
+        for i in range(1,self.N):
+            for n in range(self.number):
+                self.dep_name[n + 1] = self.dep_name_record[n + 1] + '=' + str(self.y[n][i - 1])
+            self.dep_name[0] = self.dep_name_record[0] + '=' + str(self.x[i - 1])
+            for m in range(self.number + 1):
+                exec self.dep_name[m]
+            for j in range(self.number):
+                v1[j] = eval(self.f[j])
+            for k in range(self.number):
+                self.dep_name[k + 1] = self.dep_name_record[k + 1] + '=' + str(self.y[k][i - 1] + v1[k]*self.dx/2)
+            self.dep_name[0] = self.dep_name_record[0] + '=' + str(self.x[i - 1] + self.dx/2)
+            for m in range(self.number + 1):
+                exec self.dep_name[m]
+            for j in range(self.number):
+                v2[j] = eval(self.f[j])
+            for k in range(self.number):
+                self.y[k].append(self.y[k][i - 1] + self.dx * v2[k])
+            self.x.append(self.x[i - 1] + self.dx)
+        self.data[0] =self.x
+        self.data[1] =self.y
+        return self.data
+        
+    def rgkt_3(self):                  ### third-order Runge_Kutta 
+        self.y = []
+        self.x = []
+        k1 = [0]*self.number
+        k2 = [0]*self.number
+        k3 = [0]*self.number
+        self.x.append(self.a)           #
+        for l in range(self.number):    ###
+            self.y.append([])           ####    setting the initial value
+            self.y[l].append(self.Y[l]) ###
+        for i in range(1,self.N):
+            for d in range(self.number):
+                self.dep_name[d + 1] = self.dep_name_record[d + 1] + '=' + str(self.y[d][i - 1])
+            self.dep_name[0] = self.dep_name_record[0] + '=' + str(self.x[i - 1])
+            for m in range(self.number + 1):
+                exec self.dep_name[m]
+            for j in range(self.number):
+                k1[j] = eval(self.f[j])
+            for k in range(self.number):
+                self.dep_name[k + 1] = self.dep_name_record[k + 1] + '=' + str(self.y[k][i - 1] + k1[k]*self.dx/2)
+            self.dep_name[0] = self.dep_name_record[0] + '=' + str(self.x[i - 1] + self.dx/2)
+            for m in range(self.number + 1):
+                exec self.dep_name[m]
+            for j in range(self.number):
+                k2[j] = eval(self.f[j])
+            for k in range(self.number):
+                self.dep_name[k + 1] = self.dep_name_record[k + 1] + '=' + str(self.y[k][i - 1] - k1[k]*self.dx + 2*k2[k]*self.dx)
+            self.dep_name[0] = self.dep_name_record[0] + '=' + str(self.x[i - 1] + self.dx/2)
+            for m in range(self.number + 1):
+                exec self.dep_name[m]
+            for j in range(self.number):
+                k3[j] = eval(self.f[j])
+            for k in range(self.number):
+                self.y[k].append(self.y[k][i - 1] + self.dx * (k1[k] + 4*k2[k] +k3[k])/6)
+            self.x.append(self.x[i - 1] + self.dx)
+        self.data[0] = self.x
+        self.data[1] = self.y
+        return self.data 
+ 
+    def rgkt_4(self):                  ### forth-order Runge_Kutta 
+        self.y = []
+        self.x = []
+        k1 = [0]*self.number
+        k2 = [0]*self.number
+        k3 = [0]*self.number
+        k4 = [0]*self.number
+        self.x.append(self.a)           #
+        for l in range(self.number):    ###
+            self.y.append([])           ####    setting the initial value
+            self.y[l].append(self.Y[l]) ###
+        for i in range(1,self.N):
+            for d in range(self.number):
+                self.dep_name[d + 1] = self.dep_name_record[d + 1] + '=' + str(self.y[d][i - 1])
+            self.dep_name[0] = self.dep_name_record[0] + '=' + str(self.x[i - 1])
+            for m in range(self.number + 1):
+                exec self.dep_name[m]
+            for j in range(self.number):
+                k1[j] = eval(self.f[j])
+            for k in range(self.number):
+                self.dep_name[k + 1] = self.dep_name_record[k + 1] + '=' + str(self.y[k][i - 1] + 0.5*k1[k]*self.dx)
+            self.dep_name[0] = self.dep_name_record[0] + '=' + str(self.x[i - 1] + self.dx/2)
+            for m in range(self.number + 1):
+                exec self.dep_name[m]
+            for j in range(self.number):
+                k2[j] = eval(self.f[j])
+            for k in range(self.number):
+                self.dep_name[k + 1] = self.dep_name_record[k + 1] + '=' + str(self.y[k][i - 1] + 0.5*k2[k]*self.dx)
+            self.dep_name[0] = self.dep_name_record[0] + '=' + str(self.x[i - 1] + self.dx/2)
+            for m in range(self.number + 1):
+                exec self.dep_name[m]
+            for j in range(self.number):
+                k3[j] = eval(self.f[j])
+            for k in range(self.number):
+                self.dep_name[k + 1] = self.dep_name_record[k + 1] + '=' + str(self.y[k][i - 1] + k3[k]*self.dx)
+            self.dep_name[0] = self.dep_name_record[0] + '=' + str(self.x[i - 1] + self.dx)
+            for m in range(self.number + 1):
+                exec self.dep_name[m]
+            for j in range(self.number):
+                k4[j] = eval(self.f[j])
+            for k in range(self.number):
+                self.y[k].append(self.y[k][i - 1] + self.dx * (k1[k] + 2*k2[k] + 2*k3[k] + k4[k])/6)
+            self.x.append(self.x[i - 1] + self.dx)
+        self.data[0] = self.x
+        self.data[1] = self.y
+        return self.data 
+ 
+    def store(self,data__file):           #### save the calculculate datas            
+        data_file = open(data__file,'w')
+        for i in range(self.N):
+            data_file.write(str(self.data[0][i]))
+            data_file.write(' ')
+            if (len(self.data[1] == 1)):
+               data_file.write(str(self.data[1][i]))
+            else:
+                for j in range(len(self.data[1])):
+                    data_file.write(str(self.data[1][j][i]))
+                    data_file.write(' ')
+            data_file.write('\n')
+        data_file.close  
+
